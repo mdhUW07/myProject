fced1b53a9cc8f3672fe416957accbbca084ae30
Michael Hall <hall39@wisc.edu>
2025-03-20 14:38:38 -0500

    R Script edits

--- RScripts/DistanceBased.R
+++ RScripts/DistanceBased.R
@@ -1,45 +1,26 @@
 library(adegenet)
 library(phangorn)
 library(pegas)
-
-args = commandArgs(trailingOnly=TRUE)
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).n", call.=FALSE)
-} else if (length(args)==1){
-  # default output file
- # args[2]="homologs.csv"
-}
+library(devtools)
 
 
-#dna <- fasta2DNAbin(file="outfile.fas")
-dna <- fasta2DNAbin(args[1])
 
-#Compute genetic distances using Tamura and New 1993 model which allows for different rates of transistions and transversions, heterogenous base frequencies and
-#between site variation of the substitution rate.
-print("running TN93 algorithm for Pairwise Distance Based Matrix")
-D <- dist.dna(dna, model="TN93")
-class(D)
-length(D)
+dna <- fasta2DNAbin(file="outfile.fas")
 
-print("Building Distance Matrix")
+#Compute genetic distances using Kimura 2 parameter model which allows for different rates of transitions and transversions
+
+D <- dist.dna(dna, model="K80")
+
 temp <- as.data.frame(as.matrix(D))
-print("Table.paint matrix")
-table.paint(temp,cleg=0, clabel.row=.5,clabel.col = .5)
 
+table.paint(temp,cleg=0, clabel.row=.5,clabel.col = .5)
 
 #Darker shades of grey represent greater distances.
 
-print("Building a neighbor joining tree")
 tre <- nj(D)
-class(tre)
-print("ladderize tree")
+
 tre <- ladderize(tre)
 
-print("Plotting Tree")
 plot(tre, cex=.6)
-title("A Simple Neigbor Joining Tree for Terrestris Bombus")
 
 
-#MyTreetreesMyTreetreesMyTreetreesMyTree <- read.tree("outfile.dnd",keep.multi = TRUE)
-#png("my_file_png")
-#plot(MyTree)
--- RScripts/Haplotypes.R
+++ RScripts/Haplotypes.R
@@ -1,37 +1,40 @@
 library(adegenet)
-#install.packages("phangorn",dep=TRUE)
+library(ggtree)
 library(phangorn)
 library(pegas)
+library(ggplot2)
+
 #The data used in this practical are 120 DNA sequences of Bombus terrestris dalmatinus the buff tailed bumblebee of the mitochondrial cytochrom b gene sequence from the research paper.
 #Alignments have been realized before hand using standard tools (Clustalw for basic alignment)
 
-args = commandArgs(trailingOnly=TRUE)
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).n", call.=FALSE)
-} else if (length(args)==1){
-  # default output file
-  args[2]="homologs.csv"
-}
+dna <- fasta2DNAbin(file="outfile.fas")
 
-print("Reading multiple sequence file")
-dna <- fasta2DNAbin(args[1])
-
-print("Making a Haplotype network")
 
 h <- haplotype(dna)
-h
-print("Making a Haplonet object")
+
+#Fit Kimura 2 parameter model
+D_K81 = dist.dna(h,model="K80")
+tree <- nj(D_K81)
+ggt <- ggtree::ggtree(tree,cex = 0.8, aes(color=branch.length))+scale_color_continuous(high='lightskyblue1',low='coral4')+geom_tiplab(align=TRUE, size=2)+geom_treescale(y = - 5, color = "coral4", fontsize = 4)
+njmsaplot<-msaplot(ggt,h,offset = 0.009, width=1, height = 0.5, color = c(rep("rosybrown", 1), rep("sienna1", 1), rep("lightgoldenrod1", 1), rep("lightskyblue1", 1)))
+njmsaplot
+
+#Fit Kimura 2 parameter model with mutltiple DNA sequences
+D_K81 = dist.dna(dna,model="K80")
+tree <- nj(D_K81)
+ggt <- ggtree::ggtree(tree,cex = 0.8, aes(color=branch.length))+scale_color_continuous(high='lightskyblue1',low='coral4')+geom_tiplab(align=TRUE, size=2)+geom_treescale(y = - 5, color = "coral4", fontsize = 4)
+njmsaplot<-msaplot(ggt,dna,offset = 0.009, width=1, height = 0.5, color = c(rep("rosybrown", 1), rep("sienna1", 1), rep("lightgoldenrod1", 1), rep("lightskyblue1", 1)))
+njmsaplot
+
+
+#Haplonet
 hN <- haploNet(h)
 hN
-print("Making a Dataframe of 20 different haplotypes")
 
-HapTyp_DF = as.data.frame(diffHaplo(h,1:20))
 
-print("Transposing the DataFrame")
-
+#Dataframe of 20 different haplotypes
+HapTyp_DF = as.data.frame(diffHaplo(h,1:20,strict = FALSE))
 HapTyp_DF = t(HapTyp_DF)
+View(HapTyp_DF)
 
-#View(HapTyp_DF)
-print("Writing Dataframe to CSV file")
-write.table(HapTyp_DF,file = args[2])
 
